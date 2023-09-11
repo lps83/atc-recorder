b3880e6de8aa1cb9bf0bd795636d65c83666c536
Sylvain <email@sylvain-pierson.fr>
2023-09-10 21:26:45 -0700

    Alpha

--- process_audio.sh
+++ process_audio.sh
@@ -16,10 +16,8 @@ cd output/$FOLDER_NAME
 while true; do
 
     TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-
     ffmpeg -loglevel error -hide_banner -nostats -i ${URL} -acodec libmp3lame -ar 44100 -ac 2 -t ${DURATION} "temp_recording_${TIMESTAMP}.mp3"
     ffmpeg -loglevel error -hide_banner -nostats -i "temp_recording_${TIMESTAMP}.mp3" -af "silenceremove=start_periods=1:stop_periods=-1:start_threshold=${SILENCE_THRESHOLD}:stop_threshold=${SILENCE_THRESHOLD}:start_silence=${SILENCE_DURATION}:stop_silence=${SILENCE_DURATION}" "recording_${TIMESTAMP}.mp3"
-
     rm "temp_recording_${TIMESTAMP}.mp3"
 
 done
